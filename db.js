68192c7b99bcee060b656d449615eed80525e5e6
steeven <stevenragaven1234@gmail.com>
2023-08-27 16:36:26 +0400

    Your commit message here

---
+++ db.js
@@ -0,0 +1,11 @@
+const sales = require("./data/sales");
+const sale = require("./data/sale");
+const store = require("./data/store");
+const user = require("./data/user");
+
+module.exports = {
+    store,
+    sales,
+    sale,
+    user
+}
